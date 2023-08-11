8518182655f4c5a2671a9f06a3f0430fa8769a83
SEBPO-MasudurRahman <115811230+SEBPO-MasudurRahman@users.noreply.github.com>
2023-08-11 21:59:58 +0600

    Add files via upload

--- Product_Carousel_4/health_and_logivity.js
+++ Product_Carousel_4/health_and_logivity.js
@@ -4,7 +4,7 @@
    --------------------------*/
 // tab Carousel js
 // initialize product scroller
-advanceHealth__longevityArrows("brandName-health__longevity-product");
+advanceHealth__longevityArrows("brandName-xxxx");
 function advanceHealth__longevityArrows(idx) {
     var splideElement = "#" + idx;
     var splideDefaultOptions = {
@@ -47,44 +47,5 @@ function advanceHealth__longevityArrows(idx) {
     };
 
     new Splide(splideElement, splideDefaultOptions).mount();
-}
-
-/* ************************************************************************************** */
-//for tab sweater & loader added
-document.addEventListener("DOMContentLoaded", function () {
-    filterHealth__longevityProducts("brandName-health__longevity-product");
-});
-// for dog filter function
-function filterHealth__longevityProducts(c) {
-    var x, i;
-    x = document.getElementsByClassName("citrusX__healthLogvity__product");
-    if (c == "all") c = "";
-    for (i = 0; i < x.length; i++) {
-        RemoveClass(x[i], "citrusTabShow");
-        if (x[i].className.indexOf(c) > -1) AddClass(x[i], " citrusTabShow");
-    }
-    advanceHealth__longevityArrows(c);
-}
-function AddClass(element, name) {
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for (i = 0; i < arr2.length; i++) {
-        if (arr1.indexOf(arr2[i]) == -1) {
-            element.className += " " + arr2[i];
-        }
-    }
-}
-
-function RemoveClass(element, name) {
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for (i = 0; i < arr2.length; i++) {
-        while (arr1.indexOf(arr2[i]) > -1) {
-            arr1.splice(arr1.indexOf(arr2[i]), 1);
-        }
-    }
-    element.className = arr1.join(" ");
 }
 
