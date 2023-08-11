23ca3ae933bd37c11a07c9043096e4b41e8f993d
SEBPO-MasudurRahman <115811230+SEBPO-MasudurRahman@users.noreply.github.com>
2023-08-11 17:49:05 +0600

    Add files via upload

---
+++ Product_Carousel_8/couple_scroller.js
@@ -0,0 +1,47 @@
+/**---------------------- * XXX product scroller js *-----------------------------*/
+productScrollerXXXX("xxxx__productScroller");
+function productScrollerXXXX(idx) {
+    var splideElement = "#" + idx;
+    var splideDefaultOptions = {
+        arrows: true,
+        type: "slide",
+        start: 0,
+        focus: 0,
+        pagination: false,
+        rewindSpeed: 500,
+        speed: 500,
+        pauseOnHover: true,
+        perPage: 6,
+        perMove: 1,
+        gap: 20,
+        omitEnd: true,
+        breakpoints: {
+            375: {
+                perPage: 1,
+            },
+            576: {
+                perPage: 1,
+            },
+            991: {
+                perPage: 3,
+            },
+            992: {
+                perPage: 3,
+            },
+            1024: {
+                perPage: 4,
+            },
+            1200: {
+                perPage: 6,
+            },
+            1440: {
+                perPage: 6,
+            },
+        },
+
+    };
+
+    new Splide(splideElement, splideDefaultOptions).mount();
+}
+/**---------------------- * end XXXX product scroller js *-----------------------------*/
+
